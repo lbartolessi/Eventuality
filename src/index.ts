5067709e0e0116e9270ad02e7ae79c81cc8a0d1d
Luis Bartolessi <lbartolessi@zoho.eu>
2025-06-12 02:20:10 +0100

    Fix error en RequestEvent definition

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export type { BaseEvent } from './BaseEvent.js';
 export type { EventualityInterface } from './EventualityInterface.js';
 export type { EventHandler } from './EventHandler.js';
-export type { RequestEvent } from './RequestEvent.js';
-export type { RequestEventData } from './RequestEvent.js';
+export type { RequestEvent, RequestEventData } from './RequestEvent.js';
 export type { EventualityOptions } from './EventualityOptions.js';
 export { Eventuality } from './Eventuality.js';
